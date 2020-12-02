48a10584359f12f28830d4f49442691b30d6a852
07051094 <lemon402164@gmail.com>
2020-12-02 12:49:22 +0800

    new file:   mcsio.py

---
+++ mcsio.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import time
+import sys
+import http.client as http
+import urllib
+import Adafruit_DHT
+import json
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+deviceId = "Dm4i6hW0"
+deviceKey = "04yn48Zp50wmeXSn"
+switch=1
+def post_to_mcs(payload): 
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
+		try:
+			conn = http.HTTPConnection("api.mediatek.com:80")
+			conn.connect() 
+			not_connected = 0 
+		except (http.HTTPException, socket.error) as ex: 
+			print ("Error: %s" % ex)
+			time.sleep(10)
+			
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	conn.close()
+ 
+while True:
+	h0, t0= Adafruit_DHT.read_retry(11, 4)
+	
+	switch = GPIO.input(23)
+	if switch == 1:
+		switch=0
+		print('Button pressed')
+	else:
+		switch=1
+		print('Button released')
+
+	if h0 is not None and t0 is not None:
+		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
+		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
+			{"dataChnId":"Temperature","values":{"value":t0}},
+			{"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]}  
+		post_to_mcs(payload)
+
+	else:
+		print('Failed to get reading. Try again!')
+		sys.exit(1)
